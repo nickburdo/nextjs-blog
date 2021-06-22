7103a49ac17df0840a52325e84885b303a08f575
Mykola Burdo <burdo.mb@gbksoft.com>
2021-06-22 08:14:46 +0300

    Nextjs Blog

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First post</title>
+      </Head>
+      <h1>First Post</h1>
+      <p>This is my first post content</p>
+    </Layout>
+    )
+}
